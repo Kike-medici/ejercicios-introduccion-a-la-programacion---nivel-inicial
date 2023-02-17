912887255d95efac9264c68ceaf1309df3ddbb4d
Kike-medici <125318420+Kike-medici@users.noreply.github.com>
2023-02-16 21:29:16 -0300

    Add files via upload

---
+++ bucle for.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args)
+    {
+       //for(int valor=0;valor<=3;valor++)
+       //{
+          // System.out.println(valor);
+      // }
+
+
+
+
+       //int contador[]=new int [5];
+        //o
+
+        int contador[]={10,20,30,40,50};
+
+        for(int i=0;i<contador.length;i++)
+        {
+            System.out.println(contador[i]);
+
+        }
+
+
+    }
+}
